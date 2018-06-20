7d8b7e8ccb2ebe77c4073f798d95e6bed7d24251
Rhianna Goguen <rhianna.goguen@dal.ca>
2018-06-20 14:51:03 -0300

    Commiting unfinished recyclerview for course viewing so I can switch branches RG PM

--- UniversityCourseRegistration/app/src/main/java/com/example/zacco/universitycourseregistration/AcademicTimetable.java
+++ UniversityCourseRegistration/app/src/main/java/com/example/zacco/universitycourseregistration/AcademicTimetable.java
@@ -1,28 +1,49 @@
 package com.example.zacco.universitycourseregistration;
 
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
+
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class AcademicTimetable extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+//    private RecyclerViewAdapter recyclerViewAdapter;
+    private TextView courseContent;
+    private DatabaseReference databaseReference;
+    private List<Task> allTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Query query = FirebaseDatabase.getInstance()
+
+
+        recyclerView = findViewById(R.id.recycler);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+      /*  Query query = FirebaseDatabase.getInstance()
                 .getReference()
                 .child("Course")
                 .limitToLast(50);
@@ -40,11 +61,13 @@ public class AcademicTimetable extends AppCompatActivity {
 
             @Override
             public CourseHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-                final View view = LayoutInflater.from(parent.getContext()).inflate(viewType, parent, false);
+                TextView view = (TextView) LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.activity_academic_timetable, parent, false);
                 return new CourseHolder(view);
             }
 
         };
+*/
 
-    }
+
 }
